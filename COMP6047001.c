3e57a5165c7cc47a22d091ca887ca957bb8dc12b
KenDraqxic <158826184+KenDraqxic@users.noreply.github.com>
2025-09-01 13:25:17 +0700

    Add files via upload

--- COMP6047001.c
+++ COMP6047001.c
@@ -1,4 +1,6 @@
-Printf(Nama saya William, saya berumur 18 tahun);
-Printf(Bahasa C adalah bahasa pemrograman favorit saya dan bahasa pemograman yang saya baru pelajari);
+    int main() {
+
+printf("Nama saya William, saya berumur 18 tahun.\n");
+printf("Bahasa C adalah bahasa pemrograman favorit saya dan bahasa pemograman yang saya baru pelajari.\n");
     return 0;
 }
